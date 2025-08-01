7cc36c7af0a97ceec81cb6aaac6b3a42e5fdcd12
royalcourtbd <sayedvai08@gmail.com>
2025-08-01 18:28:04 +0600

    Add customizable user input field widget
    Introduces a reusable UserInputField widget with custom input decoration and a custom text selection toolbar. This modularizes input field logic and styling, allowing for flexible configuration of icons, borders, error states, and input formatters.

---
+++ lib/core/external_libs/user_input_field/src/input_decoration.dart
@@ -0,0 +1,133 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+InputDecoration userInputDecoration({
+  required BuildContext context,
+  required String hintText,
+  required BorderRadius borderRadius,
+  TextStyle? hintStyle,
+  bool enabled = true,
+  String? suffixIconPath,
+  Color? suffixIconColor,
+  String? prefixIconPath,
+  EdgeInsetsGeometry? contentPadding,
+  VoidCallback? onTapSuffixIcon,
+  Color? prefixIconColor,
+  double? borderWidth,
+  Color? borderColor,
+  Color? focusedBorderColor,
+  Color? enabledBorderColor,
+  Color? disabledBorderColor,
+  Color? fillColor,
+  String? errorText,
+}) {
+  final ThemeData theme = Theme.of(context);
+  final TextTheme textTheme = theme.textTheme;
+
+  return InputDecoration(
+    enabled: enabled,
+    border: outlineInputBorder(
+      context: context,
+      borderRadius: borderRadius,
+      borderWidth: borderWidth ?? 0.8,
+      borderColor:
+          errorText != null ? Colors.red : (borderColor ?? Colors.transparent),
+    ),
+    focusedBorder: outlineInputBorder(
+      context: context,
+      borderRadius: borderRadius,
+      borderWidth: borderWidth ?? 0.8,
+      borderColor:
+          errorText != null
+              ? Colors.red
+              : (focusedBorderColor ?? borderColor ?? Colors.transparent),
+    ),
+    enabledBorder: outlineInputBorder(
+      context: context,
+      borderRadius: borderRadius,
+      borderWidth: borderWidth ?? 0.8,
+      borderColor:
+          errorText != null
+              ? Colors.red
+              : (enabledBorderColor ?? borderColor ?? Colors.transparent),
+    ),
+    disabledBorder: outlineInputBorder(
+      context: context,
+      borderRadius: borderRadius,
+      borderWidth: borderWidth ?? 0.8,
+      borderColor:
+          errorText != null
+              ? Colors.red
+              : (disabledBorderColor ?? borderColor ?? Colors.transparent),
+    ),
+    contentPadding: contentPadding ?? const EdgeInsets.all(5),
+    hintText: hintText,
+    filled: true,
+    fillColor: fillColor ?? theme.inputDecorationTheme.fillColor,
+    hintStyle:
+        hintStyle ??
+        textTheme.labelSmall?.copyWith(
+          fontWeight: FontWeight.w400,
+          fontSize: 14,
+          color: Colors.black38,
+        ),
+    suffixIcon:
+        suffixIconPath != null
+            ? GestureDetector(
+              onTap: () {
+                if (onTapSuffixIcon != null) {
+                  onTapSuffixIcon();
+                }
+              },
+              child: SvgPicture.asset(
+                suffixIconPath,
+                fit: BoxFit.scaleDown,
+                height: 22,
+                width: 22,
+                colorFilter: ColorFilter.mode(
+                  suffixIconColor ?? theme.primaryColor,
+                  BlendMode.srcIn,
+                ),
+              ),
+            )
+            : null,
+    prefixIcon:
+        prefixIconPath != null && prefixIconPath.isNotEmpty
+            ? Padding(
+              padding: const EdgeInsets.all(5),
+              child: SvgPicture.asset(
+                prefixIconPath,
+                fit: BoxFit.scaleDown,
+                height: 22,
+                width: 22,
+                colorFilter: ColorFilter.mode(
+                  prefixIconColor ?? theme.colorScheme.primary,
+                  BlendMode.srcIn,
+                ),
+              ),
+            )
+            : Padding(
+              padding: const EdgeInsets.all(5),
+              child: Icon(
+                Icons.search,
+                size: 22,
+                color: prefixIconColor ?? theme.colorScheme.primary,
+              ),
+            ),
+  );
+}
+
+OutlineInputBorder outlineInputBorder({
+  required BuildContext context,
+  required BorderRadius borderRadius,
+  double? borderWidth,
+  Color? borderColor,
+}) {
+  return OutlineInputBorder(
+    borderRadius: borderRadius,
+    borderSide: BorderSide(
+      width: borderWidth ?? 0.8,
+      color: borderColor ?? Colors.transparent,
+    ),
+  );
+}
---
+++ lib/core/external_libs/user_input_field/src/user_input_field_widget.dart
@@ -0,0 +1,140 @@
+import 'package:dhaka_bus/core/external_libs/user_input_field/src/custom_text_selection_toolbar.dart';
+import 'package:dhaka_bus/core/external_libs/user_input_field/src/input_decoration.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class UserInputField extends StatelessWidget {
+  const UserInputField({
+    super.key,
+    required this.textEditingController,
+    required this.hintText,
+    this.onTapSuffixIcon,
+    this.maxLength = 50,
+    this.onChanged,
+    this.borderRadius,
+    this.prefixIconPath,
+    this.prefixIconColor,
+    this.inputFormatters,
+    this.contentPadding,
+    this.onFieldSubmitted,
+    this.isError = false,
+    this.errorBorderColor,
+    this.focusNode,
+    this.fillColor,
+    this.keyboardType,
+    this.hintStyle,
+    this.borderColor,
+    this.borderWidth,
+    this.focusedBorderColor,
+    this.enabledBorderColor,
+    this.disabledBorderColor,
+    this.suffixIconPath,
+    this.suffixIconColor,
+  });
+
+  final TextEditingController textEditingController;
+
+  final String hintText;
+
+  final BorderRadius? borderRadius;
+
+  final void Function(String)? onChanged;
+
+  final void Function(String)? onFieldSubmitted;
+
+  final String? prefixIconPath;
+
+  final Color? prefixIconColor;
+
+  final String? suffixIconPath;
+
+  final Color? suffixIconColor;
+
+  final EdgeInsetsGeometry? contentPadding;
+
+  final List<TextInputFormatter>? inputFormatters;
+
+  final bool isError;
+
+  final Color? errorBorderColor;
+
+  final FocusNode? focusNode;
+
+  final Color? fillColor;
+
+  final TextInputType? keyboardType;
+
+  final TextStyle? hintStyle;
+
+  final Color? borderColor;
+
+  final double? borderWidth;
+
+  final Color? focusedBorderColor;
+
+  final Color? enabledBorderColor;
+
+  final Color? disabledBorderColor;
+
+  final VoidCallback? onTapSuffixIcon;
+
+  final int maxLength;
+
+  @override
+  Widget build(BuildContext context) {
+    final ThemeData theme = Theme.of(context);
+
+    final bool hasLengthFormatter =
+        inputFormatters?.any(
+          (formatter) => formatter is LengthLimitingTextInputFormatter,
+        ) ??
+        false;
+
+    final List<TextInputFormatter> formatters = [
+      if (!hasLengthFormatter) LengthLimitingTextInputFormatter(maxLength),
+      ...(inputFormatters ?? []),
+    ];
+
+    return TextFormField(
+      style: theme.textTheme.titleMedium!.copyWith(fontWeight: FontWeight.w400),
+      focusNode: focusNode,
+      onTapOutside: (event) => FocusScope.of(context).unfocus(),
+      cursorColor: theme.colorScheme.primary,
+      keyboardType: keyboardType ?? TextInputType.text,
+      contextMenuBuilder: (context, editableTextState) {
+        return CustomTextSelectionToolbar(
+          anchors: editableTextState.contextMenuAnchors,
+          editableTextState: editableTextState,
+        );
+      },
+      controller: textEditingController,
+      onChanged: onChanged,
+      onFieldSubmitted: onFieldSubmitted,
+      inputFormatters: formatters,
+      decoration: userInputDecoration(
+        context: context,
+        hintText: hintText,
+        contentPadding: contentPadding ?? const EdgeInsets.only(right: 15),
+        prefixIconColor: prefixIconColor ?? theme.colorScheme.primary,
+        prefixIconPath: prefixIconPath ?? '',
+        suffixIconPath: suffixIconPath,
+        suffixIconColor: suffixIconColor,
+        onTapSuffixIcon: onTapSuffixIcon,
+        fillColor: fillColor ?? theme.inputDecorationTheme.fillColor,
+        borderRadius: borderRadius ?? BorderRadius.circular(25),
+        borderColor: isError ? (errorBorderColor ?? Colors.red) : borderColor,
+        borderWidth: borderWidth,
+        focusedBorderColor: isError
+            ? (errorBorderColor ?? Colors.red)
+            : (focusedBorderColor ?? borderColor),
+        enabledBorderColor: isError
+            ? (errorBorderColor ?? Colors.red)
+            : (enabledBorderColor ?? borderColor),
+        disabledBorderColor: isError
+            ? (errorBorderColor ?? Colors.red)
+            : (disabledBorderColor ?? borderColor),
+        hintStyle: hintStyle,
+      ),
+    );
+  }
+}

a4551327abe6d2411e13079d3ebc9bde01523995
MD ABU SAYED <62033575+royalcourtbd@users.noreply.github.com>
2025-08-04 15:20:21 +0600

    Update svg_path.dart

--- lib/core/static/svg_path.dart
+++ lib/core/static/svg_path.dart
@@ -6,6 +6,9 @@ class SvgPath {
 
   ///svg file
   static const String icHomeOutline = 'assets/svg/ic_home_outline.svg';
+  static const String icHomeFilled = 'assets/svg/ic_home_fill.svg';
+  static const String icBusOutline = 'assets/svg/ic_bus_outline.svg';
+  static const String icBusFilled = 'assets/svg/ic_bus_fill.svg';
   static const String icBack = 'assets/svg/ic_back.svg';
   static const String icSearch = 'assets/svg/ic_search.svg';
   static const String icCross = 'assets/svg/ic_cross.svg';

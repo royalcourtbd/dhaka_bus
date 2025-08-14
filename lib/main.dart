2ca35b724b224f765f5121aac43a6dd24b047957
royalcourtbd <sayedvai08@gmail.com>
2025-08-15 02:47:43 +0600

    Update splash screens and launcher icons
    Replaced JPEG splash assets with PNG versions for better quality and consistency across platforms. Updated Android and iOS splash/background images, changed splash screen background color, and added adaptive launcher icons. Modified main.dart to use flutter_native_splash for splash management.

--- lib/main.dart
+++ lib/main.dart
@@ -3,12 +3,17 @@ import 'package:flutter/material.dart';
 import 'package:dhaka_bus/core/di/service_locator.dart';
 import 'package:dhaka_bus/features/app_management/domain/usecase/determine_first_run_use_case.dart';
 import 'package:dhaka_bus/dhaka_bus_app.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 
 Future<void> main() async {
   runZonedGuarded<Future<void>>(() async {
     WidgetsFlutterBinding.ensureInitialized();
+    FlutterNativeSplash.preserve(
+      widgetsBinding: WidgetsFlutterBinding.ensureInitialized(),
+    );
     await _initializeApp();
     runApp(InitialApp(isFirstRun: await _checkFirstRun()));
+    FlutterNativeSplash.remove();
   }, (error, stackTrace) => (error, stackTrace, fatal: true));
 }
 

7a7e6b4016f0b2a3a383c2e4ba598f87f23b3bf7
royalcourtbd <sayedvai08@gmail.com>
2025-08-27 01:21:09 +0600

    Add release signing config and enable ProGuard
    This commit loads keystore properties from key.properties, adds a release signing configuration, and updates the release build type to use it. It also enables code shrinking and resource shrinking with ProGuard for release builds.

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -10,12 +10,20 @@ plugins {
 }
 
 import java.util.Properties
+import java.io.FileInputStream
 
 // Load local.properties
 val localProperties = Properties()
 val localPropertiesFile = rootProject.file("local.properties")
 if (localPropertiesFile.exists()) {
     localProperties.load(localPropertiesFile.reader())
+}
+
+// Load key.properties
+val keystoreProperties = Properties()
+val keystorePropertiesFile = rootProject.file("key.properties")
+if (keystorePropertiesFile.exists()) {
+    keystoreProperties.load(FileInputStream(keystorePropertiesFile))
 }
 
 // Get version code from local.properties
@@ -52,15 +60,36 @@ android {
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = 23
         targetSdk = flutter.targetSdkVersion
+        multiDexEnabled = true
         versionCode = flutterVersionCode
         versionName = flutterVersionName
     }
+
+    signingConfigs {
+        create("release") {
+        keyAlias = keystoreProperties["keyAlias"] as String?
+        keyPassword = keystoreProperties["keyPassword"] as String?
+        storeFile = keystoreProperties["storeFile"]?.let { file(it) }
+        storePassword = keystoreProperties["storePassword"] as String?
+    }
+    }
+
+    
 
     buildTypes {
         release {
             // TODO: Add your own signing config for the release build.
             // Signing with the debug keys for now, so `flutter run --release` works.
-            signingConfig = signingConfigs.getByName("debug")
+            // signingConfig = signingConfigs.getByName("debug")
+            isMinifyEnabled = true
+            isShrinkResources = true
+            signingConfig = signingConfigs.getByName("release")
+           proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+
+
         }
     }
       // === ABI Splits Configuration ===
